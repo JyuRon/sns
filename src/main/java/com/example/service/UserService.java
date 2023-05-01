9f80399b728483a2e93ccc628835bedb742f1c92
Jyuka <yoyksks3@gmail.com>
2023-05-01 19:13:29 +0900

    회원가입 서비스 로직 구현

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import com.example.constant.ErrorCode;
+import com.example.exception.SnsApplicationException;
+import com.example.dto.UserDto;
+import com.example.domain.UserAccount;
+import com.example.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserAccountRepository userEntityRepository;
+
+    public UserDto join(String userName, String password){
+        userEntityRepository.findByUserName(userName)
+                .ifPresent(it -> {
+                    throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
+                })
+        ;
+
+        UserAccount userAccount = userEntityRepository.save(UserAccount.of(userName, password));
+
+        return UserDto.fromEntity(userAccount);
+    }
+
+}
