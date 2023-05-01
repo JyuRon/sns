ea68f6507a60677ac1f0fb50474815d873bad357
Jyuka <yoyksks3@gmail.com>
2023-05-01 19:08:03 +0900

    Custom Exception 작성

---
+++ src/main/java/com/example/constant/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.example.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
---
+++ src/main/java/com/example/exception/SnsApplicationException.java
@@ -0,0 +1,20 @@
+package com.example.exception;
+
+import com.example.constant.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class SnsApplicationException extends RuntimeException{
+    private final ErrorCode errorCode;
+    private final String message;
+
+    @Override
+    public String getMessage() {
+        if(message == null){
+            return errorCode.getMessage();
+        }
+        return String.format("%s %s", errorCode.getMessage(), message);
+    }
+}
