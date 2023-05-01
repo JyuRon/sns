27db6785b3f0b1c6ea4bb48853411b3d732edc12
Jyuka <yoyksks3@gmail.com>
2023-05-01 20:57:48 +0900

    패스워드 암호화 및 로그인(jwt token 생성) 서비스 구현

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -5,7 +5,9 @@ import com.example.exception.SnsApplicationException;
 import com.example.dto.UserDto;
 import com.example.domain.UserAccount;
 import com.example.repository.UserAccountRepository;
+import com.example.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,12 @@ public class UserService {
 
     private final UserAccountRepository userEntityRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
 
     @Transactional
     public UserDto join(String userName, String password){
@@ -31,14 +39,24 @@ public class UserService {
     }
 
     public String login(String userName, String password){
-        UserAccount userEntity = userEntityRepository.findByUserName(userName)
-                .orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,""));
+        // 회원 여부 확인
+        UserAccount userAccount = userEntityRepository.findByUserName(userName)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND,String.format("%s not founded",userName)));
 
-        if(userEntity.getPassword().equals(password)){
-            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
+        // 패스워트 확인
+        if(!passwordEncoder.matches(password, userAccount.getPassword())){
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
+        // 토큰생성
+        String token = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
 
-        return "";
+
+        return token;
+    }
+
+     void settingJwtValues(String key, Long expiredTimeMs){
+        this.secretKey = key;
+        this.expiredTimeMs = expiredTimeMs;
     }
 }
