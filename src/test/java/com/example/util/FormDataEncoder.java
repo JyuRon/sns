24ca5db502e6570b0b0d87d9e3d19bf381762472
Jyuka <yoyksks3@gmail.com>
2023-05-01 18:39:09 +0900

    테스트 코드 : Object Mapper - Post RequestBody 관련 util 추가

---
+++ src/test/java/com/example/util/FormDataEncoder.java
@@ -0,0 +1,43 @@
+package com.example.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+@TestComponent
+public class FormDataEncoder {
+
+    private final ObjectMapper mapper;
+
+    public FormDataEncoder(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+
+    public String encode(Object obj) {
+        Map<String, String> fieldMap = mapper.convertValue(obj, new TypeReference<>() {});
+        MultiValueMap<String, String> valueMap = new LinkedMultiValueMap<>();
+        valueMap.setAll(fieldMap);
+
+        return UriComponentsBuilder.newInstance()
+                .queryParams(valueMap)
+                .encode()
+                .build()
+                .getQuery();
+    }
+
+   public String objectToJson(Object obj) throws JsonProcessingException {
+        return mapper.writeValueAsString(obj);
+   }
+
+    public String objectToPrettyJson(Object obj) throws JsonProcessingException {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+    }
+
+}
