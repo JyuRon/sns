5d17efd1240da21e16f5f41d28c171f615f4500e
Jyuka <yoyksks3@gmail.com>
2023-05-02 17:21:43 +0900

    UserDto implements UserDetails

--- src/main/java/com/example/dto/UserDto.java
+++ src/main/java/com/example/dto/UserDto.java
@@ -5,12 +5,17 @@ import com.example.constant.UserRole;
 import com.example.domain.UserAccount;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
-public class UserDto {
+public class UserDto implements UserDetails {
 
     private Long id;
     private String userName;
@@ -30,5 +35,39 @@ public class UserDto {
                 entity.getUpdatedAt(),
                 entity.getDeletedAt()
         );
+    }
+
+    public static UserDto of(Long id, String userName, String password, UserRole userRole, LocalDateTime registerAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        return new UserDto(id, userName, password, userRole, registerAt, updatedAt, null);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.getUserRole().toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return deletedAt == null;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return deletedAt == null;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return deletedAt == null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return deletedAt == null;
     }
 }
