864802bea8ff74584d41cb199243a4f51eb0f20f
Jyuka <yoyksks3@gmail.com>
2023-05-02 17:23:05 +0900

    Jwt 인증으로 인한 테스트 전용 SecurityConfig 적용

---
+++ src/test/java/com/example/config/TestSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.config;
+
+import com.example.constant.UserRole;
+import com.example.dto.UserDto;
+import com.example.service.UserService;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+
+    @MockBean
+    private UserService userService;
+
+    // Spring에서 제공하는 테스트 메서드로 스프링 관련(인증) 테스트를 진행할때만 사용 가능
+    @BeforeTestMethod
+    public void SecuritySetup(){
+        given(userService.loadUserByUserName(anyString()))
+                .willReturn(createUserAccountDto());
+
+    }
+
+    private UserDto createUserAccountDto(){
+        return UserDto.of(
+                1L,
+                "jyukaTest",
+                "pw",
+                UserRole.USER,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                null
+        );
+    }
+}
--- src/test/java/com/example/controller/PostControllerTest.java
+++ src/test/java/com/example/controller/PostControllerTest.java
@@ -1,28 +1,36 @@
 package com.example.controller;
 
 import com.example.config.SecurityConfig;
+import com.example.config.TestSecurityConfig;
 import com.example.dto.request.PostCreatRequest;
+import com.example.service.PostService;
 import com.example.util.FormDataEncoder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import({FormDataEncoder.class, SecurityConfig.class})
+@Import({FormDataEncoder.class, TestSecurityConfig.class})
 @WebMvcTest(PostController.class) // Controller Test 기법, 특정 클래스 지정 없을 경우 모든 controller 호출
 class PostControllerTest {
 
     private final MockMvc mockMvc;
     private final FormDataEncoder formDataEncoder;
+
+    @MockBean
+    private PostService postService;
 
 
     PostControllerTest(
@@ -40,6 +48,7 @@ class PostControllerTest {
         // Given
         String title = "title";
         String body = "body";
+        willDoNothing().given(postService).create(anyString(),anyString(),anyString());
 
         // When & Then
         mockMvc
@@ -60,6 +69,7 @@ class PostControllerTest {
         // Given
         String title = "title";
         String body = "body";
+        willDoNothing().given(postService).create(anyString(),anyString(),anyString());
 
         // When & Then
         mockMvc
