0a26b5ed0b74fa35312241492d0b4b67895dcd0c
Jyuka <yoyksks3@gmail.com>
2023-05-04 16:00:30 +0900

    api 호출시에 토큰이 필요한데 모든 요청에 대해 토큰 filter 타는 현상 수정

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,6 +35,12 @@ public class SecurityConfig {
                 .exceptionHandling(exceptionConfig -> exceptionConfig.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
                 .build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().regexMatchers("^(?!/api/).*");
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
