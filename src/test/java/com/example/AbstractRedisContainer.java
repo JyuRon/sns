74980663bdade5d94736b13f7fe2524fc57642c7
Jyuka <yoyksks3@gmail.com>
2023-05-11 13:30:04 +0900

    kafka 관련 의존성, 테스트환경, 로컬환경 구축

--- src/test/java/com/example/AbstractRedisContainer.java
+++ src/test/java/com/example/AbstractRedisContainer.java
@@ -2,10 +2,14 @@ package com.example;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 public abstract class AbstractRedisContainer {
     static final GenericContainer MY_REDIS_CONTAINER;
+    static KafkaContainer MY_KAFKA_CONTAINER;
+
 
     static{
         MY_REDIS_CONTAINER = new GenericContainer<>("redis:6")
@@ -15,6 +19,8 @@ public abstract class AbstractRedisContainer {
 
         System.setProperty("spring.redis.host",MY_REDIS_CONTAINER.getHost());
         System.setProperty("spring.redis.port",MY_REDIS_CONTAINER.getMappedPort(6379).toString());
+
+        MY_KAFKA_CONTAINER = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
     }
 
 }
